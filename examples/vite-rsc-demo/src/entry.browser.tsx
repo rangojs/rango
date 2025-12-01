30821bd662454902a9c27d4313fe74cf6e65ee84
Ivo Todorov <ivo.gtodorov@gmail.com>
2025-12-01 05:23:10 +0200

    fix: ensure content is a Promise in RouteContentWrapper and update renderSegments logic

--- examples/vite-rsc-demo/src/entry.browser.tsx
+++ examples/vite-rsc-demo/src/entry.browser.tsx
@@ -92,7 +92,7 @@ async function initializeApp() {
     <React.StrictMode>
       <NavigationProvider
         store={store}
-        initialPayload={{ ...initialPayload, root: initialTree }}
+        initialPayload={{ ...initialPayload, root: await initialTree }}
         bridge={navigationBridge}
       />
     </React.StrictMode>
